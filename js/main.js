7b4a1bc36d0ae4f780e26312bec369f231fd1ed1
Mafuzur Rahman <programmingislove999@gmail.com>
2023-01-28 18:14:58 +0600

    rechange all

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ nullHandleForThisLocalStorageProperty('mode', {
     "modeClassName": "bg-light",
     "modeSelectedIndex": 0
 })
-var defAznSrc = 'callup/audio/Azan0.mp3'
-var defAlrmSrc = 'callup/audio/Alarm0.mp3'
+var defAznSrc = 'audio/Azan0.mp3'
+var defAlrmSrc = 'audio/Alarm0.mp3'
 nullHandleForThisLocalStorageProperty('defaultAzanTone', {
     nthNumberSelected: 0,
     src: defAznSrc
@@ -1035,13 +1035,13 @@ jQuery(document).ready(function ($) {
     }
     let o = {
         nthNumberSelected: 0,
-        src: 'callup/audio/Alarm0.mp3'
+        src: 'audio/Alarm0.mp3'
     }
     $('#settingSelectRingtoneAzan, #settingSelectRingtoneAlarm').on('change', function () {
         const s = this.selectedIndex
         o.nthNumberSelected = s
         const its = (this.id).includes('Alarm') ? 'Alarm' : 'Azan'
-        const sr = `callup/audio/${its + s}.mp3`
+        const sr = `audio/${its + s}.mp3`
         o.src = sr
         its === 'Azan' ? azanAudio.src = sr : alarmAudio.src = sr
         localStorage.setItem(`default${its}Tone`, JSON.stringify(o))
@@ -1343,7 +1343,3 @@ setTimeout(() => {
 
 
 
-//  just remaining to do---
-// 1 >> helps in html
-// 2 >> reducing variable name and increease performance
-// 3 >> styling properly
